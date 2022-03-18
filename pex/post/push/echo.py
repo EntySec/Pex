1f6aeadc3405dd991631d715d9d5501e8a6d8455
Ivan Nikolsky <enty8080@gmail.com>
2022-03-18 11:33:11 +0400

    Update echo.py

--- pex/post/push/echo.py
+++ pex/post/push/echo.py
@@ -24,6 +24,8 @@
 # SOFTWARE.
 #
 
+from alive_progress import alive_bar
+
 
 class Echo:
     @staticmethod
@@ -41,14 +43,17 @@ class Echo:
         size = len(data)
         num_parts = int(size / echo_max_length) + 1
 
-        for i in range(0, num_parts):
-            current = i * echo_max_length
-            block = self.bytes_to_octal(data[current:current + echo_max_length])
+        with alive_bar(num_parts, ctrl_c=False, title="Pushing") as bar:
+            for i in range(0, num_parts):
+                bar()
 
-            if block:
-                command = echo_stream.format(block, location)
+                current = i * echo_max_length
+                block = self.bytes_to_octal(data[current:current + echo_max_length])
 
-                if isinstance(args, dict):
-                    sender(command, **args)
-                else:
-                    sender(*args, command)
+                if block:
+                    command = echo_stream.format(block, location)
+
+                    if isinstance(args, dict):
+                        sender(command, **args)
+                    else:
+                        sender(*args, command)
