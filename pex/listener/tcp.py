ee000e1671f451aabb174d01566ff7d88b984637
Ivan Nikolsky <enty8080@gmail.com>
2022-03-26 10:35:33 +0400

    Update tcp.py

--- pex/listener/tcp.py
+++ pex/listener/tcp.py
@@ -79,5 +79,5 @@ class TCPListen:
 
 class TCPListener:
     @staticmethod
-    def listener(host, port, timeout=10):
+    def listen_tcp(host, port, timeout=10):
         return TCPListen(host, port, timeout)
