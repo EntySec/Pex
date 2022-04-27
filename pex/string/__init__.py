f3691bd0e11f97fdf63f956f80c162feb9fb31e2
Ivan Nikolsky <enty8080@gmail.com>
2022-04-27 09:50:03 +0300

    Update __init__.py

--- pex/string/__init__.py
+++ pex/string/__init__.py
@@ -24,4 +24,56 @@
 # SOFTWARE.
 #
 
-from .tools import StringTools
+import base64
+import random
+import re
+import string
+
+from itertools import cycle
+
+from .bit_reader import BitReader
+from .ring_list import RingList
+from .lzs_decompress import LZSDecompress
+
+
+class String:
+    @staticmethod
+    def extract_strings(binary_data):
+        strings = re.findall("[^\x00-\x1F\x7F-\xFF]{4,}", binary_data)
+        return strings
+
+    @staticmethod
+    def xor_string(string):
+        result = ""
+        for c in string:
+            result += chr(ord(c) ^ len(string))
+        return result
+
+    @staticmethod
+    def xor_key_string(string, key):
+        return ''.join([chr(ord(a) ^ ord(b)) for a, b in zip(string, cycle(key))])
+
+    @staticmethod
+    def xor_key_bytes(buffer, key):
+        return bytes([a ^ b for a, b in zip(buffer, cycle(key))])
+
+    @staticmethod
+    def base64_decode(string, decode=True):
+        string = base64.b64decode(string)
+        return string.decode() if decode else string
+
+    @staticmethod
+    def base64_string(string, encoded=False):
+        if not encoded:
+            string = string.encode()
+        string = base64.b64encode(string)
+        return string.decode()
+
+    @staticmethod
+    def random_string(length=16, alphabet=string.ascii_letters + string.digits):
+        return "".join(random.choice(alphabet) for _ in range(length))
+
+    @staticmethod
+    def lzs_decompress(data, window=RingList(2048)):
+        result, window = LZSDecompress(data, window)
+        return result
