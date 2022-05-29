6dac4ec3aae35096772c51e3638aa6c72ffe4748
Ivan Nikolsky <enty8080@gmail.com>
2022-05-29 17:02:43 +0300

    Update x64.py

--- pex/craft/x64.py
+++ pex/craft/x64.py
@@ -28,12 +28,29 @@ from pex.assembler import Assembler
 
 
 class X64:
+    """ Subclass of pex.craft module.
+
+    This subclass of pex.craft module is intended in providing
+    implementations of some x64 CPU features and models.
+    """
+
     assembler = Assembler()
 
-    def assemble(self, code):
+    def assemble(self, code: str) -> bytes:
+        """ Assemble code for x64 architecture.
+
+        :param str code: code to assemble
+        :return bytes: assembled code for x64 architecture
+        """
+
         return self.assembler.assemble('x64', code)
 
-    def popad(self):
+    def popad(self) -> bytes:
+        """ Pack popad x64 assembler model.
+
+        :return bytes: packed popad x64 assembler model
+        """
+
         return self.assemble(
             """
             start:
@@ -48,7 +65,12 @@ class X64:
             """
         )
 
-    def crash(self):
+    def crash(self) -> bytes:
+        """ Pack crash x64 assembler model.
+
+        :return bytes: packed crash x64 assembler model
+        """
+
         return self.popad() + self.assemble(
             """
             xor rsp, rsp
