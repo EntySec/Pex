a1a7930d83baaa092d51315d8344e7d6f8e04b2e
Ivan Nikolsky <enty8080@gmail.com>
2022-06-10 18:05:08 +0200

    Update __init__.py

--- pex/exe/__init__.py
+++ pex/exe/__init__.py
@@ -27,3 +27,30 @@ from .elf import ELF
 from .macho import Macho
 from .dylib import Dylib
 from .pe import PE
+
+
+class EXE:
+    """ Main class of pex.exe module.
+
+    This main class of pex.exe module is intended for providing
+    some implementations of executable file manipulation methods.
+    """
+
+    @staticmethod
+    def executable_replace(data: bytes, string: str, content: bytes) -> bytes:
+        """ Replace string in executable with content.
+
+        :param bytes data: executable to replace string in
+        :param str string: string to replace with content
+        :param bytes content: content to replace string with
+        :return bytes: processed executable
+        """
+
+        content_size = len(content)
+        string_size = len(string)
+
+        string_index = data.index(string)
+
+        if content_size >= string_size:
+            return data[:string_index] + content + data[string_index + content_size:]
+        return data[:string_index] + content + data[string_index + string_size:]
