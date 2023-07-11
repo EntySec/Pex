e51534fb749587d1230c8e748e587b9c0fb4e596
Ivan Nikolsky <enty8080@gmail.com>
2023-07-11 03:38:06 +0200

    Update bit_reader.py

--- pex/string/bit_reader.py
+++ pex/string/bit_reader.py
@@ -41,7 +41,7 @@ class BitReader(object):
         :return None: None
         """
 
-        super().__init()
+        super().__init__()
         self._bits = collections.deque()
 
         for byte in data_bytes:
