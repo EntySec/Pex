9163e7730c53b52ea2aaccfc1a3267ee41c5af7e
Ivan Nikolskiy <enty8080@gmail.com>
2024-04-09 21:39:56 +0100

    Update __init__.py

--- pex/string/__init__.py
+++ pex/string/__init__.py
@@ -241,3 +241,29 @@ class String(object):
 
         result, window = LZSDecompress(data, window)
         return result
+
+    @staticmethod
+    def hexdump(code: bytes, length: int = 16, sep: str = '.') -> list:
+        """ Dump bytes as hex.
+
+        :param bytes code: bytes to dump as hex
+        :param int length: length of each string
+        :param str sep: non-printable chars replacement
+        :return list: list of hexdump strings
+        """
+
+        src = code
+        filt = ''.join([(len(repr(chr(x))) == 3) and chr(x) or sep for x in range(256)])
+        lines = []
+
+        for c in range(0, len(src), length):
+            chars = src[c: c + length]
+            hex_ = ' '.join(['{:02x}'.format(x) for x in chars])
+
+            if len(hex_) > 24:
+                hex_ = '{} {}'.format(hex_[:24], hex_[24:])
+
+            printable = ''.join(['{}'.format((x <= 127 and filt[x]) or sep) for x in chars])
+            lines.append('{0:08x}  {1:{2}s} |{3:{4}s}|'.format(c, hex_, length * 3, printable, length))
+
+        return lines
