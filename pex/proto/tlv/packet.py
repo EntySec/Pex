3144c7a04eb293c39da25339b57b809a64098755
Ivan Nikolskiy <enty8080@gmail.com>
2024-05-26 17:41:41 +0100

    Update packet.py

--- pex/proto/tlv/packet.py
+++ pex/proto/tlv/packet.py
@@ -226,24 +226,6 @@ class TLVPacket(object):
         return self.__class__(
             buffer=self.get_raw(*args, **kwargs))
 
-    def get_from_dict(self, values: dict, *args, **kwargs) -> Any:
-        """ Get packets from query from dictionary.
-
-        :param dict values: type as key, value type as item
-        (e.g. for int, it should be any instance of int)
-        :return Any: any result
-        """
-
-        for type, value in values.items():
-            if isinstance(value, str):
-                return self.get_string(type, *args, **kwargs)
-            elif isinstance(value, int):
-                return self.get_int(type, *args, **kwargs)
-            elif isinstance(value, self.__class__):
-                return self.get_tlv(type, *args, **kwargs)
-            else:
-                return self.get_raw(type, values[value])
-
     def add_raw(self, type: int, value: bytes) -> None:
         """ Add raw data to packet.
 
