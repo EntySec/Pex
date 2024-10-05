6163571c49764a78ec84fadbd121508a3cf80292
Ivan Nikolskiy <enty8080@gmail.com>
2024-10-05 22:11:28 +0100

    Update client.py

--- pex/proto/snmp/client.py
+++ pex/proto/snmp/client.py
@@ -22,7 +22,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
-from pysnmp.entity.rfc3413.oneliner import cmdgen
+from pysnmp.entity.rfc3413 import cmdgen
 
 
 class SNMPClient(object):
